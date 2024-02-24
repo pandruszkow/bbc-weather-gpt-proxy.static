976bcff46c2ceb0480bf3c7d7cd60fd23cbb0d77
Piotr Andruszkow <github@andruszkow.com>
2024-02-24 11:19:02 +0800

    aa

--- bbc-grabber.py
+++ bbc-grabber.py
@@ -23,18 +23,25 @@ def parse_weather_data(json):
     print(f"Human readable location: { hr_location }")
     print("next 48 hours")
     forecasts = extract_and_flatten_forecast_objects(json)
+    forecasts = forecasts
     print(f"forecasts for: { forecasts.keys() }")
 
 def extract_and_flatten_forecast_objects(json):
     extracted_data = {}
     for forecast in json['forecasts']:
-        # Iterate over the "detailed" list in the current forecast
-        for report in forecast['detailed']["reports"]:
+        processing_limit = 6 + 24 + 24 # We want to only grab data up to 05:59 today + the next 48 hours. No sense grabbing more than that.
+        for report in forecast['detailed']["reports"][0:2]:
             # Create a new key for the current report by concatenating the local date and time slot
             key = f"{report['localDate']}T{report['timeslot']}"
 
             # Add the current report to the extracted data dictionary with the new key
             extracted_data[key] = report
+            
+            # if we're beyond the point of stopping processing, stop the parsing loop
+            --processing_limit
+            if processing_limit <= 0:
+                break
+            
     return extracted_data
         
 
@@ -44,4 +51,3 @@ weather_data = grab_weather_data(location_id)
 
 if weather_data:
     parse_weather_data(weather_data)
-    print(weather_data)  # This will print the nested dictionary
