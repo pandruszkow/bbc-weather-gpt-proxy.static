c9aa964c073e8569240d8badad27dd972635969b
Piotr Andruszkow <github@andruszkow.com>
2024-02-24 11:08:35 +0800

    aa

--- bbc-grabber.py
+++ bbc-grabber.py
@@ -28,8 +28,10 @@ def parse_weather_data(json):
 def extract_and_flatten_forecast_objects(json):
     extracted_data = {}
     for forecast in json['forecasts']:
+        print(f"Forecast obj: {forecast}")
         # Iterate over the "detailed" list in the current forecast
         for report in forecast['detailed']:
+            print(f"Report obj: {report}")
             # Create a new key for the current report by concatenating the local date and time slot
             key = f"{report['localdate']}T{report['timeslot']}"
 
